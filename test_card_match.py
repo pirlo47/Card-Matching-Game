6bce2939c64c7952dad976c383e814ee31d8da21
Phehello Ntoahae <phntoahjhb024@student.wethinkcode.co.za>
2024-11-11 15:46:54 +0200

    added new test cases

--- test_card_match.py
+++ test_card_match.py
@@ -4,6 +4,7 @@ from card_match import *
 
 class TestCardMatch(unittest.TestCase):
 
+    deck = board_grid()
     #Test for generating an even number of unique cards 
     def test_board_for_16_unique_pairs(self):
         deck = board_grid()
@@ -19,17 +20,24 @@ class TestCardMatch(unittest.TestCase):
             self.assertEqual(deck.count(card), 2)
     
     #Test for selecting cards 
-    def setup(self):
-        #initialize a deck and match testing for two cards 
-        self.deck = board_grid()
-        self.matched_positions = {0, 2} #Assume cards at position  zero and 2 are matched 
+    # def test_(self\\):
+    #     #initialize a deck and match testing for two cards 
+    #     self.deck = board_grid()
+    #     self.matched_positions = {0, 2} #Assume cards at position  zero and 2 are matched 
 
     #test for valid selection  
     def test_for_valid_selection(self):
-        result = select_card(self.deck , 1, 3, self.matched_position)
+        deck = board_grid()
+        matched_position= {0, 2}
+        
+        result = select_card(deck , 1, 3, matched_position)
         self.assertIsInstance(result, tuple, "Valid selection should return a tuple of two cards")
         self.assertEqual(len(result), 2, "Two cards should be returned for a valid selection")
-        
+    
+    #Test for a selection that is out of bounds
+    def test_out_of_bounds(self):
+        result = select_card(self.deck, -1, 3, self.macthed_positions)
+        self.assertAlmostEqual(result, "Invalid selection: Out of bounds")
        
 
 
